2a55cc83790394bb2d74c3a2e80cca0908b629ac
FBruynbroeck <francois@affinitic.be>
2015-07-29 15:06:54 +0200

    Fix autoupdate when false

--- manifests/user.pp
+++ manifests/user.pp
@@ -12,7 +12,7 @@ define ohmyzsh::user(
     if $plugins {
       ohmyzsh::plugins { $username: plugins => $plugins }
     }
-    if $autoupdate {
+    if $autoupdate != undef {
       ohmyzsh::autoupdate { $username: autoupdate => $autoupdate }
     }
   }
